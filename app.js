657902e224cf6f52b203f0b9add851ef267375f3
wantaek <wantaek@kakao.com>
2021-04-01 23:57:10 +0900

    fix app.js

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const http = require('http');
 
 const hostname = 'localhost';
-const port = process.env.port || 5000; 
+const port = process.env.PORT || 5000; 
 
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
@@ -9,6 +9,6 @@ const server = http.createServer((req, res) => {
   res.end('Hello World !!');
 });
 
-server.listen(port, () => {
+server.listen(port, hostname, () => {
   console.log(`Server running at http://${hostname}:${port}/`);
 });
