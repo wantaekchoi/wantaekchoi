94b567218c63c9aa0e96ae9076181701d0a813dd
wantaek <wantaek@kakao.com>
2021-04-02 00:04:10 +0900

    add procfile

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ const http = require('http');
 const hostname = 'localhost';
 const port = process.env.PORT || 5000; 
 
+console.log("!!");
+
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
